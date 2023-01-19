7481f567c55afa1c4e2f85030f03871b18a76826
abdojs <wat1.1taki@gmail.com>
2023-01-19 19:55:22 +0100

    test function selectById

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import { inputText } from "./FunctionText/text.js";
+import { checkElement } from "./functions/CheckElement/CheckElement.js";
+import { selectById } from "./functions/SelectById/SelectByID.js";
+
+// const res = selectById(undefined)
+// const res = selectById(null)
+// const res = selectById(12)
+// const res = selectById(function () { console.log(""); });
+// const res = selectById([]);
+// const res = selectById("undefined")
+// const res = selectById(submit);
+const submit = selectById("submit-test")
+
+
+console.log(submit);
