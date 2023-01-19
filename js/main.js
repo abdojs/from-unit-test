c6cbcb90d77767f9da03c675c4e3cee2c36378d1
abdojs <wat1.1taki@gmail.com>
2023-01-19 21:04:32 +0100

    config

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import { checkElement } from "./functions/CheckElement/CheckElement.js";
-import { selectById } from "./functions/SelectById/SelectByID.js";
+// import { checkElement } from "./functions/CheckElement/CheckElement.js";
+// import { selectById } from "./functions/SelectById/SelectByID.js";
+import { string } from "./helpers/string/string.js";
 
 // const res = selectById(undefined)
 // const res = selectById(null)
@@ -8,9 +9,10 @@ import { selectById } from "./functions/SelectById/SelectByID.js";
 // const res = selectById([]);
 // const res = selectById("undefined")
 // const res = selectById(submit);
+// const submit = selectById("submit-test")
+// const check = checkElement(submit)
 
-const submit = selectById("submit-test")
-const check = checkElement(submit)
+const abd = string("hshshshshshs");
 
 
-console.log(check);
+console.log(abd);
