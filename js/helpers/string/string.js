884aedc23b727cdcdefa8bb197823d0b674e143c
abdojs <wat1.1taki@gmail.com>
2023-01-19 21:04:10 +0100

    check text in string or not string

---
+++ js/helpers/string/string.js
@@ -0,0 +1,20 @@
+/**
+ * function check text in string or not string
+ * @param {String} text 
+ * @returns {Boolean} true or false
+ */
+const string = (text) => {
+
+    if (!(typeof text === "string"))
+        return false;
+    return true;
+
+
+}
+
+
+export {
+    string,
+}
+
+
